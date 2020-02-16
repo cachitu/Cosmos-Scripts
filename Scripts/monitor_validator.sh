6462682bd427ec56cad4254ddb5650e26ea1070a
Calin Chitu <calin.chitu@yahoo.fr>
2020-02-16 23:06:07 +0200

    Update monitor_validator.sh

--- Scripts/monitor_validator.sh
+++ Scripts/monitor_validator.sh
@@ -3,13 +3,15 @@ while true
 do
 last_block=`gaiacli status | jq '.sync_info.latest_block_height'`
 curl localhost:26657/validators?height=$last_block | jq '.result.validators' > validators.json
-VP=`cat validators.json | jq ' .[] | select(.address=="D43F2F5CF003859AB7319A1C760AA6A05572B214").voting_power | tonumber'`
-echo "Voting power: "$VP
-if [ $VP -gt 0 ]
+VP=`cat validators.json | jq ' .[] | select(.address=="2B89383448ACC1DD6B870385C16342F0F3950C31").voting_power | tonumber'`
+CS=`curl localhost:26657/dump_consensus_state -s | grep 2B89383448AC | wc -l`
+echo "Consenus count: $CS, Voting power: "$VP
+if [ $VP -gt 0 ] && [ $CS -gt 2 ]
 then
-echo "good: we have voting power: "$VP
+echo "good: at block $last_block we have voting power: "$VP
 else
-echo "bad: no voting power"
+echo "bad: no voting power or cs count"
+/home/test/telegram.sh "No voting power for gaiad testnet at block $last_block"
 fi
 sleep 15
 done
