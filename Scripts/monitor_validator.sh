3c3a8e53caa8708beefcbe49a1823ef14e8a2ac5
Calin Chitu <calin.chitu@yahoo.fr>
2020-03-08 08:50:41 +0200

    Update monitor_validator.sh

--- Scripts/monitor_validator.sh
+++ Scripts/monitor_validator.sh
@@ -1,17 +1,25 @@
 VP=1
+ERCOUNT=0
 while true
 do
 last_block=`gaiacli status | jq '.sync_info.latest_block_height'`
-curl localhost:26657/validators?height=$last_block | jq '.result.validators' > validators.json
-VP=`cat validators.json | jq ' .[] | select(.address=="2B89383448ACC1DD6B870385C16342F0F3950C31").voting_power | tonumber'`
-CS=`curl localhost:26657/dump_consensus_state -s | grep 2B89383448AC | wc -l`
-echo "Consenus count: $CS, Voting power: "$VP
+curl -s localhost:26657/validators?height=$last_block | jq '.result.validators' > validators.json
+VP=`cat validators.json | jq ' .[] | select(.address=="BF4CB4D59D19D451CF5E7BC49349DF4AA222D78B").voting_power | tonumber'`
+CS=`curl localhost:26657/dump_consensus_state -s | grep BF4CB4D59D19 | wc -l`
 if [ $VP -gt 0 ] && [ $CS -gt 2 ]
 then
-echo "good: at block $last_block we have voting power: "$VP
+ERCOUNT=0
 else
-echo "bad: no voting power or cs count"
-/home/test/telegram.sh "No voting power for gaiad testnet at block $last_block"
+echo "Bad: no voting power or cs count. Err count: $ERRCOUNT"
+if [ $ERCOUNT -gt 2 ]
+then
+echo "Send alert. Err count: $ERCOUNT"
+/home/kytzu/telegram.sh "No voting power for gaiad mainnet kytzu at block $last_block. Error count: $ERCOUNT"
+ERCOUNT=$((ERCOUNT+1))
+else
+echo "Increase ercount: $ERCOUNT"
+ERCOUNT=$((ERCOUNT+1))
 fi
-sleep 15
+fi
+sleep 8
 done
